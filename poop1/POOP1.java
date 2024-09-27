72f5287baaf000c94e090d77c3a3d1bb205aa0cd
FamSet <aramarfdo@gmail.com>
2024-09-27 17:57:32 -0600

    Add files via upload

---
+++ poop1/POOP1.java
@@ -0,0 +1,13 @@
+package poop1;
+public class POOP1 {
+public static void main(String[] args) { 
+System.out.println("Hola Mundo con ANT"); 
+System.out.println("Desde POO G3"); 
+int a = 3; 
+Integer b = 3; 
+String cadena = new String("hola"); 
+System.out.println(a); //Parseo 
+System.out.println(b); //Parseo 
+System.out.println(cadena); //Nada 
+} 
+}
